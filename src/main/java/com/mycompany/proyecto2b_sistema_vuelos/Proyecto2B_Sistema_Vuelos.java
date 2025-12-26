cd9c2d13920423c48ea21cf6546ef938ac92b70a
Usuario <Usuario@DESKTOP-6ODNHNK>
2025-12-26 15:59:35 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/proyecto2b_sistema_vuelos/Proyecto2B_Sistema_Vuelos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto2b_sistema_vuelos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Proyecto2B_Sistema_Vuelos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
